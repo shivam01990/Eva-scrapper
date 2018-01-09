fb5d3ebd8d15f04a91c9d1d2af263c34cb367291
Shivam Srivastava <shivam01990@gmail.com>
2018-01-09 10:01:04 +0530

    recoverycompassScrapper complete

--- FencingScrapper/FencingScrapper/Scrapper/recoverycompassScrapper.cs
+++ FencingScrapper/FencingScrapper/Scrapper/recoverycompassScrapper.cs
@@ -22,14 +22,116 @@ namespace FencingScrapper.Scrapper
             string outhtml = (string)sender;
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(outhtml);
+            HtmlNodeCollection items = doc.DocumentNode.SelectNodes("//ul[contains(@class, 'job_listings')]//li");
+            foreach (var item in items)
+            {
+                scrapperModel model = new scrapperModel();
+
+                HtmlNode anchortag = item.SelectSingleNode(".//div[contains(@class, 'content-box')]//a");
+
+                if (anchortag != null)
+                {
+                    try
+                    {
+                        model.DetailsPageUrl = anchortag.GetAttributeValue("href", string.Empty).Replace("\n", " ").Replace("\r", " ").Replace("&amp;", " ").Replace("&nbsp;", " ");                     
+                    }
+                    catch { }
+                }
+
+                modelData.Add(model);
+            }
         }
 
         public void ExtractData()
         {
+            //MainWindow browser = new MainWindow(GetUrl(), this);
             ChromeBrowserWindow browser = new ChromeBrowserWindow(GetUrl(), this);
             browser.ShowDialog();
-           // GenrateReport.StartGenerate("recoverycompass", modelData);
+            GetSubPageData(modelData);
+            GenrateReport.StartGenerate("recoverycompass", modelData);
         }
+
+        private void GetSubPageData(List<scrapperModel> modeldata)
+        {
+            foreach (var model in modeldata)
+            {
+                try
+                {
+                    if (model.DetailsPageUrl != string.Empty)
+                    {
+                        Console.WriteLine("Grabbing data for suburl" + model.DetailsPageUrl);
+                        string outhtml = Helper.GetHtmlFromUrl(model.DetailsPageUrl);
+                        HtmlDocument doc = new HtmlDocument();
+                        doc.LoadHtml(outhtml);
+
+                        try
+                        {
+                            HtmlNode titlenode = doc.DocumentNode.SelectSingleNode("//h1[contains(@class, 'job_listing-title')]");
+                            if (titlenode != null)
+                            {
+                                model.CompanyName = titlenode.InnerText.Replace("\n", " ").Replace("\r", " ").Replace("\t", "").Replace("&amp;", " ");
+                            }
+                        }
+                        catch { }
+
+                        try
+                        {
+                            HtmlNode phone = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'job_listing-phone')]");
+                            if (phone != null)
+                            {
+                                model.Phone = phone.InnerText.Replace("\n", " ").Replace("\r", " ").Replace("\t", "").Replace("CALL OR TEXT:", " ");
+                            }
+                        }
+                        catch { }
+
+                        try
+                        {
+
+                            HtmlNode addressnode = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'job_listing-location-formatted')]");
+                            if (addressnode != null)
+                            {
+                                model.Address = addressnode.InnerText.Replace("\n", " ").Replace("\r", " ").Replace("\t", "").Replace("&amp;", " ");
+                                KeyValuePair<string, string> citynstate = Helper.GetStateAndCity(model.Address);
+                                model.City = citynstate.Key;
+                                model.State = citynstate.Value;
+                            }
+                        }
+                        catch
+                        { }
+
+
+                        try
+                        {
+                            HtmlNode website = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'job_listing-url')]");
+                            if (website != null)
+                            {
+                                model.CompanyUrl = website.InnerText.Replace("\n", " ").Replace("\r", " ").Replace("\t", "").Replace("&amp;", " ");
+                            }
+                        }
+                        catch { }
+
+                        try
+                        {
+
+                            HtmlNode pricenode = doc.DocumentNode.SelectSingleNode("//div[contains(@id, 'jmfe-custom-price')]");
+                            if (pricenode != null)
+                            {
+                                model.Prices = pricenode.InnerText.Replace("\n", " ").Replace("\r", " ").Replace("\t", "").Replace("&amp;", " ");
+                               
+                            }
+                        }
+                        catch
+                        { }
+
+                        model.IsDetailsPageScrapped = true;
+                        //ScrapperProvider.SaveScrapper(model);
+
+                    }
+                }
+                catch { Helper.AddtoLogFile("Error in" + model.DetailsPageUrl); }
+            }
+        }
+
 
         public string GetUrl()
         {
