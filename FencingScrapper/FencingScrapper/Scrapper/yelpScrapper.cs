a1bd3ead3ed4f9481130677134e34cb8e016c71e
Shivam Srivastava <shivam01990@gmail.com>
2018-01-11 09:04:11 +0530

    Yelp changes

--- FencingScrapper/FencingScrapper/Scrapper/yelpScrapper.cs
+++ FencingScrapper/FencingScrapper/Scrapper/yelpScrapper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FencingScrapper.DB;
 
 namespace FencingScrapper.Scrapper
 {
@@ -15,57 +16,57 @@ namespace FencingScrapper.Scrapper
         {
             return new List<string>()
             {
-                "AK",
-                //"AL",
-                //"AR",
-                //"AZ",
-                //"CA",
-                //"CO",
-                //"CT",
-                //"DC",
-                //"DE",
-                //"FL",
-                //"GA",
-                //"HI",
-                //"IA",
-                //"ID",
-                //"IL",
-                //"IN",
-                //"KS",
-                //"KY",
-                //"LA",
-                //"MA",
-                //"MD",
-                //"ME",
-                //"MI",
-                //"MN",
-                //"MO",
-                //"MS",
-                //"MT",
-                //"NC",
-                //"ND",
-                //"NE",
-                //"NH",
-                //"NJ",
-                //"NM",
-                //"NV",
-                //"NY",
-                //"OH",
-                //"OK",
-                //"OR",
-                //"PA",
-                //"RI",
-                //"SC",
-                //"SD",
-                //"TN",
-                //"TX",
-                //"UT",
-                //"VA",
-                //"VT",
-                //"WA",
-                //"WI",
-                //"WV",
-                //"WY"
+                //"AK", // Done
+                //"AL", // Done
+                //"AR", //Done
+                //"AZ", //Done
+                //"CA", //Done
+                //"CO", //Done
+                //"CT", //Done
+                //"DC", //Done
+                //"DE", //Done
+                //"FL", //Done
+                //"GA", //Done
+                //"HI", //Done
+                //"IA", //Done
+                //"ID", //Done
+                //"IL", //DOne
+                //"IN", //Done
+                //"KS", //Done
+                //"KY", //Done
+                //"LA",   //Done
+                //"MA",   //Done
+                //"MD",   //Done
+                //"ME", //Done
+                //"MI", //Done
+                //"MN", //Done
+                //"MO", //Done
+                //"MS", //Done
+                //"MT", //Done
+                //"NC", //Done
+                //"ND", //Done
+                //"NE", //Done
+                //"NH", //Done
+                //"NJ", //Done
+                //"NM", //Done
+                //"NV", //Done
+                //"NY", //Done
+                //"OH", //Done
+                //"OK", //Done
+                "OR",
+                "PA",
+                "RI",
+                "SC",
+                "SD",
+                "TN",
+                "TX",
+                "UT",
+                "VA",
+                "VT",
+                "WA",
+                "WI",
+                "WV",
+                "WY"
             };
         }
 
@@ -77,10 +78,7 @@ namespace FencingScrapper.Scrapper
         private string GetUrl(int pageNo, string states, string service)
         {
             int startPointer = pageNo * 10;
-            if (pageNo > 0)
-            {
-                startPointer = startPointer - 10;
-            }
+            startPointer = startPointer - 10;
             return GetUrl(states, service) + "&start=" + startPointer;
         }
 
@@ -89,31 +87,89 @@ namespace FencingScrapper.Scrapper
             return GetUrl() + "search?find_desc=" + service + "&find_loc=" + states;
         }
 
-        private void GetDataForState(string service, string state)
+        private List<scrapperModel> GetDataForState(string service, string state)
         {
-            int totalPages = 0;
-            int currentPage = 0;
-            do
+            int totalPages = 1;
+            int currentPage = 1;
+            List<scrapperModel> modelData = new List<scrapperModel>();
+
+            while (currentPage <= totalPages)
             {
 
                 Console.WriteLine("Grabbing data for " + GetUrl(currentPage, state, service));
                 HtmlDocument doc = Helper.GetHtmlDocFromUrl(GetUrl(currentPage, state, service));
                 HtmlNode toalPagesNode = doc.DocumentNode.SelectSingleNode("//div[@class='page-of-pages arrange_unit arrange_unit--fill']");
+                if (toalPagesNode != null)
+                {
+                    string pagingtext = toalPagesNode.InnerText.Replace("\n", "").Replace("\r", "");
+                    string totalpagestr = pagingtext.Split('f').LastOrDefault().Trim();
+                    int.TryParse(totalpagestr, out totalPages);
+                }
 
+                HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//ul//li[@class='regular-search-result']");
+                if (nodes != null)
+                {
+                    foreach (var node in nodes)
+                    {
+                        scrapperModel model = new scrapperModel();
+                        model.SourceUrl = GetUrl();
+                        HtmlNode titlenode = node.SelectSingleNode(".//h3[@class='search-result-title']//a");
+                        if (titlenode != null)
+                        {
+                            model.CompanyName = titlenode.InnerText.Replace("\n", " ").Replace("\r", " ");
+
+                            try
+                            {
+                                model.DetailsPageUrl = "https://www.yelp.com" + titlenode.GetAttributeValue("href", string.Empty).Replace("\n", " ").Replace("\r", " ").Replace("&amp;", " ").Replace("&nbsp;", " ");
+                            }
+                            catch { }
+
+                        }
+
+                        HtmlNode phonenode = node.SelectSingleNode(".//div[@class='secondary-attributes']//span[@class='biz-phone']");
+                        if (phonenode != null)
+                        {
+                            model.Phone = phonenode.InnerText.Replace("\n", "").Replace("\r", "").Trim();
+                        }
+
+
+                        HtmlNode address = node.SelectSingleNode(".//div[@class='secondary-attributes']//address");
+                        if (address != null)
+                        {
+                            model.Address = address.InnerText.Replace("\n", "").Replace("\r", "").Trim();
+                            KeyValuePair<string, string> citynstate = Helper.GetStateAndCity(model.Address);
+                            model.City = citynstate.Key;
+                            model.State = citynstate.Value;                           
+                        }
+                        model.State = state;
+                        HtmlNode price = node.SelectSingleNode(".//div[@class='main-attributes']//div[@class='media-story']//span[@class='business-attribute price-range']");
+                        if (price != null)
+                        {
+                            model.Prices = price.InnerText.Replace("\n", "").Replace("\r", "").Trim();
+                        }
+
+                        model = ScrapperProvider.SaveScrapper(model);
+                        modelData.Add(model);
+
+                    }
+
+                }
                 // HtmlNodeCollection items = doc.DocumentNode.SelectNodes("//table[contains(@class, 'table-striped')]//tr");
 
                 currentPage = currentPage + 1;
-            } while (currentPage <= totalPages);
+            }
 
+            return modelData;
         }
 
         public void ExtractData()
         {
-            foreach (string service in GetServices())
+            foreach (string state in GetStates())
             {
-                foreach (var state in GetStates())
+                foreach (var service in GetServices())
                 {
-                    GetDataForState(service, state);
+                    List<scrapperModel> modelData = GetDataForState(service, state);
+                    GenrateReport.StartGenerate("yelp_" + service.Replace("+", "") + "_" + state, modelData);
                 }
             }
         }
